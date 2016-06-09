8551fc7a5e2488421e4fe944356fd423620711e4
Mark Kennedy <mark@gamer-network.net>
2016-06-09 14:12:41 +0100

    refactor settings, will only ouptput one ad per zone

--- server/yafa/views.py
+++ server/yafa/views.py
@@ -1,7 +1,7 @@
 from yafa.models import *
 
 from django.http import JsonResponse, HttpResponse
-from datetime import datetime
+from django.utils import timezone
 
 from django.core import serializers
 from django.core.serializers.json import DjangoJSONEncoder
@@ -12,21 +12,22 @@ from hashids import Hashids
 def all(request, site):
     ads = Advert.objects.filter(
         site__slug = site,
-        end_date__gte = datetime.now()
-    #).distinct(
-    #    'zone'
+        end_date__gte = timezone.now()
     ).all()
 
-    ad_manifest = [
-        {
-            ad.zone.slug : {
-                'obfuscated_name': garble( ad.id ),
-                'image': ad.image,
-                'click': ad.click,
-            }
+    zones = Zone.objects.all()
+
+    ad_manifest = {}
+    for ad in ads:
+        # if there is more than one ad per zone, this will
+        # just pick whichever comes out last
+        # TODO: distribute?
+        ad_manifest[ad.zone.slug] = {
+            'obfuscated_name': garble( ad.id ),
+            'image': ad.image,
+            'click': ad.click,
         }
-        for ad in ads
-    ]
+
     #res = HttpResponse(serializers.serialize('json', ad_manifest), content_type='application/json')
     res = JsonResponse(ad_manifest, safe=False)
     return res
